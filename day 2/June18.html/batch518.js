0015b56d6456b5bcc1acb99fd797144c2f7415d6
surya karthikeyan <suryakarthikn16@gamil.com>
2026-06-18 11:52:08 +0530

    Java Script

---
+++ day 2/June18.html/batch518.js
@@ -0,0 +1,28 @@
+function example(){
+    console.log("B")
+}
+const example1=()=>{
+    console.log("D")
+}
+console.log("A")
+setTimeout(example,0)
+setTimeout(example1,2000)
+console.log("C")
+
+
+
+const promise=new Promise((resolve,reject)=>{
+    // const success=true;
+    // if(success){
+    //     console.log("Done")
+    // }
+    // else{
+    //     console.log("Fail")
+    // }
+    setTimeout(()=>{
+        console.log("Hi") 
+    },1000)
+})
+
+promise.then(res=>{console.log(res)})
+.catch(err=>{console.log(err)})
